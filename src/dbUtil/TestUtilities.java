4936f577c5d0574386272639e77f82398133fbf7
Kevin <hoangja@plu.edu>
2016-04-26 09:41:40 -0700

    2nd init: recent update

--- src/dbUtil/TestUtilities.java
+++ src/dbUtil/TestUtilities.java
@@ -2,10 +2,10 @@ package dbUtil;
 /**
  * This program is used to test the Utilities class
  */
- 
+
 // You need to import the java.sql package to use JDBC
 import java.sql.*; 
- 
+
 import java.util.Scanner;
 
 /**
@@ -19,7 +19,6 @@ public class TestUtilities {
 	static Scanner keyboard = new Scanner(System.in); // standard input
 
 	public static void main(String[] args) throws SQLException {
-
 		// variables needed for menu
 		int choice;
 		boolean done = false;
@@ -45,11 +44,31 @@ public class TestUtilities {
 				callEmployeeByDNO();
 				break;
 			}
+			case 5: {
+				callGetProjectDetails();
+				break;
+			}
+			case 6: {
+				callEmployeeProjectByDNO();
+				break;
+			}
+			case 7: {
+				openManual();
+				break;
+			}
+			case 8: {
+				callMatchWorkOnSameProject();
+				break;
+			}
 			case 9: {
-				testObj.closeDB(); //close the DB connection 
+				callGetEmployeeHasNoPROJECT();
 				break;
 			}
 			case 10: {
+				testObj.closeDB(); //close the DB connection 
+				break;
+			}
+			case 11: {
 				done = true;
 				System.out.println("Good bye");
 				break;
@@ -64,18 +83,19 @@ public class TestUtilities {
 		System.out.println("2)  call getNameSalary()");
 		System.out.println("3)  call matchLastName(String)");
 		System.out.println("4)  call employeeByDNO()");
-		System.out.println("5)  open ");
-		System.out.println("6)  call ");
-		System.out.println("7)  call ");
-		System.out.println("8)  call ");
-		System.out.println("9)  close the DB");
-		System.out.println("10) quit");
+		System.out.println("5)  call getProjectDetails");
+		System.out.println("6)  call employeeProjectByDNO()");
+		System.out.println("7)  open manual DB ");
+		System.out.println("8)  call matchWorkOnSameProject()");
+		System.out.println("9)  call getEmployeeHasNoPROJECT()");
+		System.out.println("10)  close the DB");
+		System.out.println("11) quit");
 	}
 
 	static int getChoice() {
 		String input;
 		int i = 0;
-		while (i < 1 || i > 10) {
+		while (i < 1 || i > 14) {
 			try {
 				System.out.print("Please enter an integer between 1-10: ");
 				input = keyboard.nextLine();
@@ -93,6 +113,15 @@ public class TestUtilities {
 		testObj.openDB();
 	}
 
+	// open the manual database;
+	static void openManual() {
+		System.out.print("Please enter username: ");
+		String username = keyboard.nextLine();
+		System.out.print("Please enter password: ");
+		String password = keyboard.nextLine();
+		testObj.openDB(username,password);
+	}
+
 	// test getNameSalary() method
 	static void callGetNameSalary() throws SQLException {
 		ResultSet rs;
@@ -102,7 +131,7 @@ public class TestUtilities {
 		rs = testObj.getNameSalary();
 		while (rs.next()) {
 			System.out.printf("%-26s %s \n", rs.getString(1) + ", " + rs.getString(2), 
-					                         rs.getString(3));
+					rs.getString(3));
 		}
 	}
 
@@ -120,8 +149,8 @@ public class TestUtilities {
 					rs.getString(2) + ", " + rs.getString(3));
 		}
 	}
-	
-	 
+
+
 	//test employeeByDNO() method 
 	static void callEmployeeByDNO() throws SQLException {
 		ResultSet rs;
@@ -135,9 +164,69 @@ public class TestUtilities {
 		while(rs.next()){ 
 			System.out.printf("    %-8s     %s\n", rs.getString(1), 
 					rs.getString(2) + ", " + rs.getString(3));
-			}
-		
+		}
+
 	}
-	
-		
+
+	//test employeeProjectByDNO method 
+	static void callEmployeeProjectByDNO() throws SQLException {
+		ResultSet rs;
+		System.out.print("Please enter a department number: ");
+		String input = keyboard.nextLine();
+		int dno= Integer.parseInt(input); 
+		System.out.println("\nEmployees that work in department " + dno); 
+		System.out.println("*******************************************");
+		System.out.printf("%s\t%s\t%s\n", "Project Number","LastName, FirstName","Hours");
+		rs = testObj.employeeProjectByDNO(dno); 
+		while(rs.next()){ 
+			System.out.printf("     %s\t\t%s\t\t%s\n", rs.getString(3), 
+					rs.getString(1) + ", " + rs.getString(2),rs.getString(4));
+		}
+
+	}	
+
+	// test getProjectDetails() method
+	static void callGetProjectDetails() throws SQLException {
+		ResultSet rs;
+		System.out.println("Project Details");
+		System.out.println("*********************************************");
+		System.out.printf("Project number\tNumber Employee\tTotal Hours\tAverage Hours\n");
+		rs = testObj.getProjectDetails();
+		while (rs.next()) {
+			System.out.printf("      %s     \t\t%s\t%s\t\t%s\n", rs.getString(1),rs.getString(2), 
+					rs.getString(3),rs.getString(4));
+		}
+	}
+
+	//test matchWorkOnSameProject method 
+	static void callMatchWorkOnSameProject() throws SQLException {
+		ResultSet rs;
+		System.out.print("Please enter employee firstname: ");
+		String fname = keyboard.nextLine();
+		System.out.print("Please enter employee lastname: ");
+		String lname = keyboard.nextLine();
+		System.out.println("\nEmployees that work in same project with " + fname + " " + lname); 
+		System.out.println("******************************************************");
+		System.out.printf("%s\t%s\t%s\n", "#_Department","LastName, FirstName","Salary");
+		rs = testObj.matchWorkOnSameProject(fname,lname); 
+		while(rs.next()){ 
+			System.out.printf("  %s\t\t%s\t\t%s\n", rs.getString(1), 
+					rs.getString(2) + ", " + rs.getString(3),rs.getString(4));
+		}
+
+	}
+
+	// test getProjectDetails() method
+	static void callGetEmployeeHasNoPROJECT() throws SQLException {
+		ResultSet rs;
+		System.out.println("Employee who does not work on any project");
+		System.out.println("*********************************************");
+		System.out.printf("Project number\tSalary\n");
+		rs = testObj.getEmployeeHasNoPROJECT();
+		while (rs.next()) {
+			System.out.printf("%s\t%s\n", rs.getString(1),rs.getString(2));
+		}
+	}
+
+
 }//MyUtilitiesTest	
--- src/dbUtil/Utilities.java
+++ src/dbUtil/Utilities.java
@@ -5,7 +5,7 @@ package dbUtil;
  * to open an modify the DB.
  * 
  */
- 
+
 
 // You need to import the java.sql package to use JDBC methods and classes
 
@@ -19,7 +19,7 @@ import java.sql.*;
 public class Utilities {
 
 	private Connection conn = null; // Connection object
-								   
+
 
 	/**
 	 * Open a MySQL DB connection where user name and password are predefined
@@ -28,24 +28,24 @@ public class Utilities {
 	public void openDB() {
 		// Load the MySQL JDBC driver
 		try {
-		Class.forName("com.mysql.jdbc.Driver");
-		//Class.forName("com.mysql.jdbc.Driver").newInstance();
+			Class.forName("com.mysql.jdbc.Driver");
+			//Class.forName("com.mysql.jdbc.Driver").newInstance();
 
 		} catch (ClassNotFoundException e) {
-		System.out.println("Unable to load driver.");
+			System.out.println("Unable to load driver.");
 		}  
 
 		// Connect to the database
 		String url = "jdbc:mysql://zoe.cs.plu.edu:3306/company367_2016";
-		String username = "xx367";
-		String password = "xx367";
-		 
+		String username = "ba367";
+		String password = "LizIsQueen";
+
 		try {
-		conn = DriverManager.getConnection(url, username, password);
+			conn = DriverManager.getConnection(url, username, password);
 		} catch (SQLException e) {
-		System.out.println("Error connecting to database: " + e.toString());
+			System.out.println("Error connecting to database: " + e.toString());
 		}
-		 
+
 	}// openDB
 
 	/**
@@ -70,13 +70,13 @@ public class Utilities {
 	public ResultSet getNameSalary() {
 		ResultSet rset = null;
 		String sql = null;
-		 
+
 		try {
 			// create a Statement and an SQL string for the statement
 			Statement stmt = conn.createStatement();
 			sql = "SELECT lname, fname, salary FROM employee, department " +
-				  "WHERE dno=dnumber and dname='Research' " +
-				  "ORDER BY lname, fname";
+					"WHERE dno=dnumber and dname='Research' " +
+					"ORDER BY lname, fname";
 			rset = stmt.executeQuery(sql);
 		} catch (SQLException e) {
 			System.out.println("createStatement " + e.getMessage() + sql);
@@ -85,7 +85,7 @@ public class Utilities {
 		return rset;
 	}// getNameSalary
 
-	
+
 	/**
 	 * This method creates an SQL statement to list fname, lname, and department 
 	 * number of all employees that have a last name that starts with 
@@ -103,19 +103,19 @@ public class Utilities {
 			// create a Statement and an SQL string for the statement
 			Statement stmt = conn.createStatement();
 			sql = "SELECT dno, lname, fname FROM employee " +
-				  "WHERE lname like '"+target+"%' " +
-				  "ORDER BY dno";
+					"WHERE lname like '"+target+"%' " +
+					"ORDER BY dno";
 			rset = stmt.executeQuery(sql);
 		} catch (SQLException e) {
 			System.out.println("createStatement " + e.getMessage() + sql);
 		}
 
 		return rset;
-		
-		
-	
+
+
+
 	}// matchLastName
-	
+
 	/**
 	 * This method creates an SQL statement to list fname, lname, and department 
 	 * number of all employees that work in the department with number
@@ -133,13 +133,13 @@ public class Utilities {
 		try {
 			// create a Statement and an SQL string for the statement
 			sql = "SELECT dno, lname, fname FROM employee " +
-			  "WHERE dno = ? " +
-			  "ORDER BY dno";
+					"WHERE dno = ? " +
+					"ORDER BY dno";
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.clearParameters();
 			pstmt.setInt(1,dno); //set the 1 parameter
-			
+
 			rset = pstmt.executeQuery();
 		} catch (SQLException e) {
 			System.out.println("createStatement " + e.getMessage() + sql);
@@ -147,8 +147,8 @@ public class Utilities {
 
 		return rset;	
 	}// matchLastName2
-	
-	
+
+
 	/******************** 1 Write and Test ********************************
 	 * Overload the open method that opens a MySQL DB with the user name and 
 	 * password given as input.
@@ -156,8 +156,28 @@ public class Utilities {
 	 * @param username is a String that is the DB account username
 	 * @param password is a String that is the password the account
 	 */
-	
-	
+	public void openDB(String username, String password) {
+		// Load the MySQL JDBC driver
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			//Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("Unable to load driver.");
+		}  
+
+		// Connect to the database
+		String url = "jdbc:mysql://zoe.cs.plu.edu:3306/company367_2016";
+
+		try {
+			conn = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			System.out.println("Error connecting to database: " + e.toString());
+		}
+
+	}// openDB(String username, String password)
+
+
 	/******************** 2 Write and Test ********************************
 	 * Write a method that returns lname, fname, project number and hours of 
 	 * all employees that work on a project controlled by department, deptNum.
@@ -167,7 +187,28 @@ public class Utilities {
 	 * @return ResultSet with lname, fname, project number and hours of 
 	 * all employees that work on a project controlled by department dno
 	 */
-	
+	public ResultSet employeeProjectByDNO(int deptNum) {
+		ResultSet rset = null;
+		String sql = null;
+
+		try {
+			// create a Statement and an SQL string for the statement
+			sql = "SELECT lname, fname, pno, hours FROM employee join works_on on ssn = essn " +
+					"WHERE dno = ? " +
+					"ORDER BY pno";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			pstmt.clearParameters();
+			pstmt.setInt(1,deptNum); //set the 1 parameter
+
+			rset = pstmt.executeQuery();
+		} catch (SQLException e) {
+			System.out.println("createStatement " + e.getMessage() + sql);
+		}
+
+		return rset;	
+	}//  employeeProjectByDNO
+
 	/******************** 3 Write and Test ********************************
 	 * Write a method that returns for each project the number of employees that 
 	 * work on the project, the total number of hours they have all worked on the 
@@ -178,8 +219,24 @@ public class Utilities {
 	 * work on the project, the total number of hours they have all worked on the 
 	 * project, and the average number of hours each employee has worked on project
 	 */
-	
-	
+	public ResultSet getProjectDetails() {
+		ResultSet rset = null;
+		String sql = null;
+
+		try {
+			// create a Statement and an SQL string for the statement
+			Statement stmt = conn.createStatement();
+			sql = "SELECT pno,count(*),sum(hours),avg(hours) FROM employee join works_on on ssn = essn " +
+					"GROUP BY pno " +
+					"ORDER BY pno";
+			rset = stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			System.out.println("createStatement " + e.getMessage() + sql);
+		}
+
+		return rset;
+	}// getProjectDetails()
+
 	/******************** 4 Write and Test ********************************
 	 * Write a method that returns fname, lname, salary, and dno for each employee
 	 * that works on a project with the employee specified by input values 
@@ -190,9 +247,26 @@ public class Utilities {
 	 * @return ResultSet that has fname, lname, salary, and dno for each employee
 	 * that works on a project with the employee empFname, empLname
 	 */
-	 
-	 
-	 /******************** 5 Write and Test ********************************
+	public ResultSet matchWorkOnSameProject(String empFname,String empLname) {
+		ResultSet rset = null;
+		String sql = null;
+
+		try {
+			// create a Statement and an SQL string for the statement
+			Statement stmt = conn.createStatement();
+			sql = "SELECT dno, lname, fname,salary FROM employee join works_on on ssn = essn " +
+					"WHERE pno IN " + 
+					"(SELECT pno FROM employee join works_on on ssn = essn where fname like '"+empFname+"%' and lname like '"+empLname+"%') " +
+					"ORDER BY dno";
+			rset = stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			System.out.println("createStatement " + e.getMessage() + sql);
+		}
+
+		return rset;	
+	}// matchWorkOnSameProject
+
+	/******************** 5 Write and Test ********************************
 	 * Retrieve the names of employees who do not work on any project and their salary
 	 * Names must be in the format "lname, fname" i.e., the last name and first name 
 	 * must be concatenated.
@@ -200,8 +274,24 @@ public class Utilities {
 	 * @return ResultSet that has employee name and salary of all employees that do
 	 *         not work on any project.
 	 */
-	 
-	  /******************** 6  Write and Test ********************************
+	public ResultSet getEmployeeHasNoPROJECT() {
+		ResultSet rset = null;
+		String sql = null;
+
+		try {
+			// create a Statement and an SQL string for the statement
+			Statement stmt = conn.createStatement();
+			sql = "SELECT concat(lname,', ',fname) Name, salary FROM employee left outer join works_on on essn = ssn " +
+					"WHERE essn is null ";
+			rset = stmt.executeQuery(sql);
+		} catch (SQLException e) {
+			System.out.println("createStatement " + e.getMessage() + sql);
+		}
+
+		return rset;
+	}// getEmployeeHasNoPROJECT()
+
+	/******************** 6  Write and Test ********************************
 	 * YOU MUST USE A PreparedStatement 
 	 * This method will use a PreparedStatement and the information in data 
 	 * to update the works_on table.  Each row of the 2-dim array, data, 
@@ -212,5 +302,6 @@ public class Utilities {
 	 * @param data is a nx3 table of Strings where each row has the format (essn, pno, hours)
 	 * @return number of tuples successfully inserted into works_on
 	 */
-	 
+
+
 }// Utilities class
